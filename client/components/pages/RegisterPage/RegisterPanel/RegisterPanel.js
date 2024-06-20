661f742feec830e5f7abba0150bf360a0da86368
renepuentes0718 <jackiecapital007@gmail.com>
2024-06-20 10:32:16 -0500

    minor change

--- client/components/pages/RegisterPage/RegisterPanel/RegisterPanel.js
+++ client/components/pages/RegisterPage/RegisterPanel/RegisterPanel.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import R from 'ramda';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import R from "ramda";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
-import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
-import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons/faTriangleExclamation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons/faTriangleExclamation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import Box from 'react-bulma-companion/lib/Box';
-import Button from 'react-bulma-companion/lib/Button';
-import Control from 'react-bulma-companion/lib/Control';
-import Element from 'react-bulma-companion/lib/Element';
-import Field from 'react-bulma-companion/lib/Field';
-import Help from 'react-bulma-companion/lib/Help';
-import Icon from 'react-bulma-companion/lib/Icon';
-import Input from 'react-bulma-companion/lib/Input';
-import Label from 'react-bulma-companion/lib/Label';
-import Title from 'react-bulma-companion/lib/Title';
+import Box from "react-bulma-companion/lib/Box";
+import Button from "react-bulma-companion/lib/Button";
+import Control from "react-bulma-companion/lib/Control";
+import Element from "react-bulma-companion/lib/Element";
+import Field from "react-bulma-companion/lib/Field";
+import Help from "react-bulma-companion/lib/Help";
+import Icon from "react-bulma-companion/lib/Icon";
+import Input from "react-bulma-companion/lib/Input";
+import Label from "react-bulma-companion/lib/Label";
+import Title from "react-bulma-companion/lib/Title";
 
-import GoogleSignUpButton from '../../../elements/SocialLogin/signUp';
+import GoogleSignUpButton from "../../../elements/SocialLogin/signUp";
 
-import { postCheckUsername } from '_api/users';
+import { postCheckUsername } from "_api/users";
 
-import { attemptRegister } from '_store/thunks/auth';
+import { attemptRegister } from "_store/thunks/auth";
 
-import useKeyPress from '_hooks/useKeyPress';
+import useKeyPress from "_hooks/useKeyPress";
 
-import { validatePassword, validateEmail } from '_utils/validation';
+import { validatePassword, validateEmail } from "_utils/validation";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 export default function RegisterPanel() {
   const dispatch = useDispatch();
 
-  const [username, setUsername] = useState('');
-  const [usernameMessage, setUsernameMessage] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordMessage, setPasswordMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
   const [usernameAvailable, setUsernameAvailable] = useState(false);
   const [passwordValid, setPasswordValid] = useState(false);
 
@@ -69,49 +69,49 @@ export default function RegisterPanel() {
   //   }
   // };
 
-  const checkEmail = email => {
-    console.log( validateEmail(email) );
+  const checkEmail = (email) => {
+    console.log(validateEmail(email));
 
-    if ( validateEmail(email) ) {
-      setUsernameMessage('Checking email...');
+    if (validateEmail(email)) {
+      setUsernameMessage("Checking email...");
       setUsernameAvailable(false);
 
       postCheckUsername(email)
-        .then(res => {
+        .then((res) => {
           setUsernameAvailable(res.available);
           setUsernameMessage(res.message);
         })
         .catch(R.identity);
     } else {
       setUsernameAvailable(false);
-      setUsernameMessage('Invalid email address.');
+      setUsernameMessage("Invalid email address.");
     }
-  }
+  };
 
-  const updateUsername = newUserName => {
+  const updateUsername = (newUserName) => {
     setUsername(newUserName);
     checkPassword(newUserName, password);
   };
 
-  const handleUsernameChange = e => {
+  const handleUsernameChange = (e) => {
     e.preventDefault();
     updateUsername(e.target.value);
     checkEmail(e.target.value);
   };
 
-  const handlePasswordChange = e => {
+  const handlePasswordChange = (e) => {
     setPassword(e.target.value);
     checkPassword(username, e.target.value);
   };
 
-  const register = (email=null, oauth = false) => {
-    if( oauth ) {
+  const register = (email = null, oauth = false) => {
+    if (oauth) {
       const newUser = {
-        username:email,
+        username: email,
         password: "1234",
       };
 
-      console.log(newUser)
+      console.log(newUser);
 
       setLoading(true);
       dispatch(attemptRegister(newUser))
@@ -130,7 +130,7 @@ export default function RegisterPanel() {
     }
   };
 
-  useKeyPress('Enter', register);
+  useKeyPress("Enter", register);
 
   return (
     <Box className={styles.root}>
@@ -140,22 +140,20 @@ export default function RegisterPanel() {
       <hr className="separator" />
       <p className="has-space-below">
         Already a member?&nbsp;
-        <Link to="/login">
-          Login
-        </Link>
+        <Link to="/login">Login</Link>
       </p>
       <Field>
-        <GoogleSignUpButton registerGoogleUser={register}/>
+        <GoogleSignUpButton registerGoogleUser={register} />
       </Field>
       <Field>
-        <Label htmlFor="username">
-          Email
-        </Label>
+        <Label htmlFor="username">Email</Label>
         <Control iconsRight>
           <Input
             id="username"
             placeholder="Email"
-            color={username ? (usernameAvailable ? 'success' : 'danger') : undefined}
+            color={
+              username ? (usernameAvailable ? "success" : "danger") : undefined
+            }
             value={username}
             type="email"
             onChange={handleUsernameChange}
@@ -164,7 +162,7 @@ export default function RegisterPanel() {
             <Icon
               size="small"
               align="right"
-              color={usernameAvailable ? 'success' : 'danger'}
+              color={usernameAvailable ? "success" : "danger"}
             >
               <FontAwesomeIcon
                 icon={usernameAvailable ? faCheck : faTriangleExclamation}
@@ -173,21 +171,21 @@ export default function RegisterPanel() {
           )}
         </Control>
         {username && (
-          <Help color={usernameAvailable ? 'success' : 'danger'}>
+          <Help color={usernameAvailable ? "success" : "danger"}>
             {usernameMessage}
           </Help>
         )}
       </Field>
       <Field>
-        <Label htmlFor="password">
-          Password
-        </Label>
+        <Label htmlFor="password">Password</Label>
         <Control iconsRight>
           <Input
             id="password"
             placeholder="Password"
             type="password"
-            color={password ? (passwordValid ? 'success' : 'danger') : undefined}
+            color={
+              password ? (passwordValid ? "success" : "danger") : undefined
+            }
             value={password}
             onChange={handlePasswordChange}
           />
@@ -195,7 +193,7 @@ export default function RegisterPanel() {
             <Icon
               size="small"
               align="right"
-              color={passwordValid ? 'success' : 'danger'}
+              color={passwordValid ? "success" : "danger"}
             >
               <FontAwesomeIcon
                 icon={passwordValid ? faCheck : faTriangleExclamation}
@@ -204,7 +202,7 @@ export default function RegisterPanel() {
           )}
         </Control>
         {password && (
-          <Help color={passwordValid ? 'success' : 'danger'}>
+          <Help color={passwordValid ? "success" : "danger"}>
             {passwordMessage}
           </Help>
         )}
